d63d0182a080133e811933584cf17f2e9099267c
01max <m.louguet@gmail.com>
2019-07-10 14:49:30 +0200

    Add Fighter factory. Update seed to create fighters.

--- spec/factories/fighter_factories.rb
+++ spec/factories/fighter_factories.rb
@@ -2,7 +2,17 @@ FactoryBot.define do
 
   factory :fighter do |c|
 
-    c.name { Faker::Address.country }
+    c.name {
+      [
+        Faker::Movies::LordOfTheRings,
+        Faker::Games::Zelda,
+        Faker::TvShows::DrWho
+      ].sample.character
+    }
+
+    c.health_base_points  { rand(0..Fighter::MAX_HEALTH_BASE_POINTS) }
+    c.defense_base_points { rand(0..Fighter::MAX_DEFENSE_BASE_POINTS) }
+    c.attack_base_points  { Fighter::MAX_TOTAL_BASE_POINTS - (health_base_points + defense_base_points) }
 
   end
 
